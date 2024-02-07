7347ec017f709d21521e97900996f29443526888
Johnny Lin <47119111+lostmypillow@users.noreply.github.com>
2024-02-08 00:05:26 +0800

    0208

---
+++ app/lobby/page.tsx
@@ -0,0 +1,6 @@
+'use client'
+export default function Heyyy() {
+    return(
+        <h1>heyyy</h1>
+    );
+}
---
+++ app/ui/fonts.tsx
@@ -0,0 +1,3 @@
+import { Noto_Sans_TC } from 'next/font/google';
+ 
+export const noto = Noto_Sans_TC({ subsets: ['latin', 'latin-ext'] });
