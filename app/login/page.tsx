b12a3a3275a152b8908dc7b740a22332836441d7
LostMyPillow <47119111+lostmypillow@users.noreply.github.com>
2024-02-08 17:05:40 +0800

    weird behavior

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,71 +1,83 @@
-'use client'
-import { TextInput, PasswordInput, Button } from '@mantine/core';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+'use client';
+import { TextInput, PasswordInput, Button } from "@mantine/core";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { IconLogin2 } from '@tabler/icons-react';
 
 function LoginPassword() {
-  return(
+  return (
     <div className="flex flex-row items-end justify-between">
       <PasswordInput
-      size="lg"
-      radius="xl"
-      label="密碼"
-      placeholder="請輸入密碼"
-      className='basis-9/12'
-    />
+        size="lg"
+        radius="xl"
+        label="密碼"
+        placeholder="請輸入密碼"
+        className="basis-9/12"/>
       {/* <div> */}
       {/* <span className="material-symbols-outlined">
           login
          
         </span> */}
-        <Link href="/lobby">Temp Lobby Link</Link>
+      {/* <Link href="/lobby">Temp Lobby Link</Link> */}
       {/* </div> */}
-      
 
-        {/* <Button variant="filled" size="lg" radius="xl" className='basis-3/12 mx-2'
-      onClick={() => router.push('/lobby')}>
-      </Button> */}
+      <Button
+        variant="filled"
+        size="md"
+        radius="xl"
+        className="basis-3/12 mx-2"
+        rightSection={<IconLogin2 size={25}/>}
+         component={Link} 
+         href="/lobby"
+      >登入</Button>
     </div>
-    
   );
-  //  
+  //
 }
 
 function LoginUsername() {
-  return(
+  return (
     <TextInput
       size="lg"
       radius="xl"
       label="帳號"
       placeholder="請輸入學號"
+      autoFocus
+      tabIndex={1}
     />
   );
 }
 
-function LoginInputs({children}: {children: React.ReactNode}) {
-  return<form className="flex flex-col gap-3">{children}</form>
+function LoginInputs({ children }: { children: React.ReactNode }) {
+  return <form className="flex flex-col h-full w-full lg:w-2/6 gap-3">{children}</form>;
 }
 
 function LoginHeader() {
-  return<h1>登入北科大!</h1>
+  return <h1 className="self-center">登入北科大!</h1>;
 }
 
-function LoginForm({children}: {children: React.ReactNode}) {
-  return(
-    <div className='flex flex-col gap-5 mx-4 lg:w-96 lg:justify-center'>{children}</div>
-  )
+function LoginForm({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col gap-4 lg:w-4/6  lg:items-center max-h-svh justify-center">
+      {children}
+    </div>
+  );
+}
+
+function LoginContainer({ children }: { children: React.ReactNode }) {
+  return <div className="flex items-center justify-center basis-11/12">{children}</div>;
 }
 
 export default function Login() {
   return (
-    <LoginForm>
-      <LoginHeader />
-      <LoginInputs>
-        <LoginUsername />
-        <LoginPassword />
-      </LoginInputs>
-    </LoginForm>
-  
-    
-      );
+    <LoginContainer>
+      <LoginForm>
+        <LoginHeader />
+        <LoginInputs>
+          <LoginUsername />
+          <LoginPassword />
+        </LoginInputs>
+      </LoginForm>
+    </LoginContainer>
+  );
 }
--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,22 +1,26 @@
 'user client'
 import Link from "next/link";
+import { Text } from "@mantine/core";
+import Image from "next/image";
 export function NavBar() {
     return (
         <div className="flex flex-row flex-nowrap items-center justify-between p-4 shrink-0 fixed h-16 max-h-16 w-full top-0 text-xl">
 
             <div id="NavLeft" className="flex items-center justify-center">
-                <Link href="/"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" /></svg>
+                <Link href="/" className="font-medium text-indigo-900 dark:text-blue-900 hover:underline hover:decoration-sky-500" style={{ textDecoration: 'none' }}>
+                    Home
                 </Link>
             </div>
 
-            <div id="NavMid" className="flex items-center justify-center">
-                <p>TT</p>
+            <div id="NavMid" className="flex grow-0 items-center justify-center">
+                <div className="font-black">
+                    <Image src="/computer.png" width={28} height={28} alt="text"></Image>
+                </div>
             </div>
 
             <div id="NavLeft" className="flex items-center justify-center">
-                <Link href="/login">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z" />
-                    </svg>
+                <Link href="/login" className="font-medium text-indigo-900 dark:text-blue-900 hover:underline hover:decoration-sky-500" style={{ textDecoration: 'none' }}>
+                    Login
                 </Link>
             </div>
 
